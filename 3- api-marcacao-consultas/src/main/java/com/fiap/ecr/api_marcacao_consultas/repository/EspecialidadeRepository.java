86824e623e65b722badd089cab7cccb8f310ac18
Mateus_Fairbanks <125223503+totagoma@users.noreply.github.com>
2025-04-22 08:14:08 -0300

    Delete 3- api-marcacao-consultas directory
    repositorio clonado e com falha sendo excluido

--- 3- api-marcacao-consultas/src/main/java/com/fiap/ecr/api_marcacao_consultas/repository/EspecialidadeRepository.java
+++
@@ -1,5 +0,0 @@
-package com.fiap.ecr.api_marcacao_consultas.repository;
-import com.fiap.ecr.api_marcacao_consultas.model.Especialidade;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long> {
-}
--- 3- api-marcacao-consultas/src/main/java/com/fiap/ecr/api_marcacao_consultas/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fiap.ecr.api_marcacao_consultas.repository;
-import com.fiap.ecr.api_marcacao_consultas.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByEmail(String email);
-}
